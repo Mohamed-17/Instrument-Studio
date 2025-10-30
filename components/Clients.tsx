61670439c8295b72fa89aca4fc59aecde354a2c3
Mohamed ashraf <142633529+Mohamed-17@users.noreply.github.com>
2025-10-30 19:52:59 +0300

    Fix border styling in Clients component

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -21,7 +21,7 @@ function Clients() {
       <section className="pb-16 md:pb-32 masked">
         <div className="group relative m-auto max-w-6xl px-6">
           <div className="flex flex-col items-center md:flex-row ">
-            <div className="md:max-w-44 md:border-r md:pr-6 border-b pb-6 md:pb-0">
+            <div className="md:max-w-44 md:border-r md:pr-6 border-b md:border-b-0 pb-6 md:pb-0">
               <p className="text-end text-sm">Powering the best teams</p>
             </div>
             <div className="relative py-10 md:w-[calc(100%-11rem)]">
